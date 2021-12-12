f5689e79451b8bef3c03fca50d4f969809e9e823
BraeWebb <email@braewebb.com>
2021-12-12 16:28:57 +1000

    Day 12

--- day12/day12.py
+++ day12/day12.py
@@ -1,21 +1,89 @@
-""""(*@ \section{Day 12: ...} @*)"""""
+""""(*@ \section{Day 12: Passage Pathing} @*)"""""
 
+from typing import List
 from util import *
+
 
 def preprocess(stdin):
     for line in stdin.splitlines():
-        yield convert_sequence(line)
+        yield line.split("-")
 
 """"(*@ \subsection{Task One} @*)"""""
 
+class Graph:
+    def __init__(self, stopping_condition) -> None:
+        self._lines = []
+        self._stopping_condition = stopping_condition
+
+    def add_line(self, start, end):
+        self._lines.append((start, end))
+        self._lines.append((end, start))
+
+    def get_nexts(self, from_) -> List[str]:
+        for start, end in self._lines:
+            if from_ == start:
+                yield end
+
+    def find_paths_from(self, start, path, seen):
+        for next in self.get_nexts(start):
+            # path finished, yield it
+            if next == "end":
+                yield path + ("end",)
+
+            # task specific rules for stopping
+            if self._stopping_condition(seen, next):
+                continue
+            
+            # update seen dictionary to add one to the next
+            now_seen = dict(seen, **{next: seen.get(next, 0) + 1})
+
+            # find all paths to 'end' from here
+            next_path = path + (next,)
+            yield from self.find_paths_from(next, next_path, now_seen)
+
+    def find_all_paths(self):
+        path_generator = self.find_paths_from(
+            "start",
+            ("start",),
+            {"start": 1}
+        )
+
+        return set(path for path in path_generator)
+
+
+def visit_small_once(seen, next):
+    if next in ("start", "end"):
+        return True
+    
+    return next.islower() and next in seen
+
 def task1(lines):
-    pass
+    graph = Graph(visit_small_once)
+    for line in lines:
+        graph.add_line(*line)
+
+    return len(graph.find_all_paths())
 
 
 """"(*@ \subsection{Task Two} @*)"""""
 
+def visit_small_twice(seen, next):
+    if next in ("start", "end"):
+        return True
+    
+    if next.islower() and next in seen:
+        for cave, visited in seen.items():
+            if cave.islower() and visited > 1:
+                return True
+    
+    return False
+
 def task2(lines):
-    pass
+    graph = Graph(visit_small_twice)
+    for line in lines:
+        graph.add_line(*line)
+
+    return len(graph.find_all_paths())
 
 
 run(globals())
---
+++ day13/day13.py
@@ -0,0 +1,20 @@
+""""(*@ \section{Day 13: ...} @*)"""""
+
+from util import *
+
+def preprocess(stdin):
+    for line in stdin.splitlines():
+        yield convert_sequence(line)
+
+""""(*@ \subsection{Task One} @*)"""""
+
+def task1(lines):
+    pass
+
+
+""""(*@ \subsection{Task Two} @*)"""""
+
+def task2(lines):
+    pass
+
+run(globals())
